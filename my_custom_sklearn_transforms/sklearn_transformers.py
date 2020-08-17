03c1ff4ba5f6580d4b9ed23d5afa72eb7ae2a9f2
Carlos Fernandes <carlos.fernandes@hotmail.com.br>
2020-08-16 22:56:39 -0300

    Adicionando Feature

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,34 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class NotasFeatureTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        data = X.copy()
+
+        # Removendo outliers
+        data["NOTA_MF"] = np.where(data["NOTA_MF"] > 10, 10, data["NOTA_MF"])
+
+        data["TAN_SQRT_NOTA_MF"] = tan(sqrt(data['NOTA_MF']))
+        data["SQRT_NOTA_MF"] = sqrt(data['NOTA_MF'])
+        data["SQRT_NOTA_DE"] = sqrt(data['NOTA_DE'])
+        data["SQRT_NOTA_EM"] = sqrt(data['NOTA_MF'])
+        data["SQRT_NOTA_GO"] = sqrt(data['NOTA_GO'])
+
+        data["D_NOTA_DE"] = np.where(data["NOTA_DE"] <= 6, 0, 1)
+        data["D_NOTA_EM"] = np.where(data["NOTA_EM"] <= 6, 0, 1)
+        data["D_NOTA_MF"] = np.where(data["NOTA_MF"] <= 6, 0, 1)
+        data["D_NOTA_GO"] = np.where(data["NOTA_GO"] <= 6, 0, 1)
+
+        data["REPROVACOES_DE"] = np.where(data["REPROVACOES_DE"] > 1, 1, data["REPROVACOES_DE"])
+        data["REPROVACOES_EM"] = np.where(data["REPROVACOES_EM"] > 1, 1, data["REPROVACOES_EM"])
+        data["REPROVACOES_MF"] = np.where(data["REPROVACOES_MF"] > 1, 1, data["REPROVACOES_MF"])
+        data["REPROVACOES_GO"] = np.where(data["REPROVACOES_GO"] > 1, 1, data["REPROVACOES_GO"])
+
+        return data
+        
