5d64d13a8d9471999f1acd0476328cb9b50f0634
Carlos Fernandes <carlos.fernandes@hotmail.com.br>
2020-08-17 00:58:44 -0300

    Config numpy

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
+import numpy
 from sklearn.base import BaseEstimator, TransformerMixin
-from numpy import tan, sqrt
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -26,23 +26,23 @@ class NotasFeatureTransformer(BaseEstimator, TransformerMixin):
         data = X.copy()
 
         # Removendo outliers
-        data["NOTA_MF"] = np.where(data["NOTA_MF"] > 10, 10, data["NOTA_MF"])
+        data["NOTA_MF"] = numpy.where(data["NOTA_MF"] > 10, 10, data["NOTA_MF"])
 
-        data["TAN_SQRT_NOTA_MF"] = tan(sqrt(data['NOTA_MF']))
-        data["SQRT_NOTA_MF"] = sqrt(data['NOTA_MF'])
-        data["SQRT_NOTA_DE"] = sqrt(data['NOTA_DE'])
-        data["SQRT_NOTA_EM"] = sqrt(data['NOTA_MF'])
-        data["SQRT_NOTA_GO"] = sqrt(data['NOTA_GO'])
+        data["TAN_SQRT_NOTA_MF"] = numpy.tan(sqrt(data['NOTA_MF']))
+        data["SQRT_NOTA_MF"] = numpy.sqrt(data['NOTA_MF'])
+        data["SQRT_NOTA_DE"] = numpy.sqrt(data['NOTA_DE'])
+        data["SQRT_NOTA_EM"] = numpy.sqrt(data['NOTA_MF'])
+        data["SQRT_NOTA_GO"] = numpy.sqrt(data['NOTA_GO'])
 
-        data["D_NOTA_DE"] = np.where(data["NOTA_DE"] <= 6, 0, 1)
-        data["D_NOTA_EM"] = np.where(data["NOTA_EM"] <= 6, 0, 1)
-        data["D_NOTA_MF"] = np.where(data["NOTA_MF"] <= 6, 0, 1)
-        data["D_NOTA_GO"] = np.where(data["NOTA_GO"] <= 6, 0, 1)
+        data["D_NOTA_DE"] = numpy.where(data["NOTA_DE"] <= 6, 0, 1)
+        data["D_NOTA_EM"] = numpy.where(data["NOTA_EM"] <= 6, 0, 1)
+        data["D_NOTA_MF"] = numpy.where(data["NOTA_MF"] <= 6, 0, 1)
+        data["D_NOTA_GO"] = numpy.where(data["NOTA_GO"] <= 6, 0, 1)
 
-        data["REPROVACOES_DE"] = np.where(data["REPROVACOES_DE"] > 1, 1, data["REPROVACOES_DE"])
-        data["REPROVACOES_EM"] = np.where(data["REPROVACOES_EM"] > 1, 1, data["REPROVACOES_EM"])
-        data["REPROVACOES_MF"] = np.where(data["REPROVACOES_MF"] > 1, 1, data["REPROVACOES_MF"])
-        data["REPROVACOES_GO"] = np.where(data["REPROVACOES_GO"] > 1, 1, data["REPROVACOES_GO"])
+        data["REPROVACOES_DE"] = numpy.where(data["REPROVACOES_DE"] > 1, 1, data["REPROVACOES_DE"])
+        data["REPROVACOES_EM"] = numpy.where(data["REPROVACOES_EM"] > 1, 1, data["REPROVACOES_EM"])
+        data["REPROVACOES_MF"] = numpy.where(data["REPROVACOES_MF"] > 1, 1, data["REPROVACOES_MF"])
+        data["REPROVACOES_GO"] = numpy.where(data["REPROVACOES_GO"] > 1, 1, data["REPROVACOES_GO"])
 
         return data
         
