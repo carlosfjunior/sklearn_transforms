0d34c23f0086f7e781dbcc6ea38473ad04f97486
carlosfjunior <carlos.fernandes@hotmail.com.br>
2020-12-05 14:11:24 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -15,7 +15,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
-class NotasFeatureTransformer(BaseEstimator, TransformerMixin):
+class CustomTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         print('Execution NotasFeatureTransformer')
 
@@ -24,25 +24,9 @@ class NotasFeatureTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         data = X.copy()
-
-        # Removendo outliers
-        data["NOTA_MF"] = numpy.where(data["NOTA_MF"] > 10, 10, data["NOTA_MF"])
-
-        data["TAN_SQRT_NOTA_MF"] = numpy.tan(numpy.sqrt(data['NOTA_MF']))
-        data["SQRT_NOTA_MF"] = numpy.sqrt(data['NOTA_MF'])
-        data["SQRT_NOTA_DE"] = numpy.sqrt(data['NOTA_DE'])
-        data["SQRT_NOTA_EM"] = numpy.sqrt(data['NOTA_MF'])
-        data["SQRT_NOTA_GO"] = numpy.sqrt(data['NOTA_GO'])
-
-        data["D_NOTA_DE"] = numpy.where(data["NOTA_DE"] <= 6, 0, 1)
-        data["D_NOTA_EM"] = numpy.where(data["NOTA_EM"] <= 6, 0, 1)
-        data["D_NOTA_MF"] = numpy.where(data["NOTA_MF"] <= 6, 0, 1)
-        data["D_NOTA_GO"] = numpy.where(data["NOTA_GO"] <= 6, 0, 1)
-
-        data["REPROVACOES_DE"] = numpy.where(data["REPROVACOES_DE"] > 1, 1, data["REPROVACOES_DE"])
-        data["REPROVACOES_EM"] = numpy.where(data["REPROVACOES_EM"] > 1, 1, data["REPROVACOES_EM"])
-        data["REPROVACOES_MF"] = numpy.where(data["REPROVACOES_MF"] > 1, 1, data["REPROVACOES_MF"])
-        data["REPROVACOES_GO"] = numpy.where(data["REPROVACOES_GO"] > 1, 1, data["REPROVACOES_GO"])
-
+        
+        data = data[data['koi_pdisposition'].notna()]
+        data = data[data['koi_score'].values < 1.1]
+        
         return data
         
