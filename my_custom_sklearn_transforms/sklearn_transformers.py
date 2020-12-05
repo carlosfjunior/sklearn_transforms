f644cbd52ed80c06c405f5c525195a762bd5e8c3
carlosfjunior <carlos.fernandes@hotmail.com.br>
2020-12-05 14:48:38 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -28,5 +28,33 @@ class CustomTransformer(BaseEstimator, TransformerMixin):
         data = data[data['koi_pdisposition'].notna()]
         data = data[data['koi_score'].values < 1.1]
         
+        koi_pdisposition
+        
         return data
+ 
+class ItemSelector():
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data_dict):
+        return data_dict[self.key]
+    
+class MyLEncoder():
+
+    def transform(self, X, y=None, **fit_params):
+        enc = preprocessing.LabelEncoder()
+        encc = enc.fit(X)
+        enc_data = enc.transform(X)
+
+        return enc_data
+
+    def fit_transform(self, X, y=None, **fit_params):
+        self.fit(X, y, **fit_params)
+        return self.transform(X)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
         
