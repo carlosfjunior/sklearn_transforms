e75ee5041c2cde71e634e966bd1b86ef33f0b797
carlosfjunior <carlos.fernandes@hotmail.com.br>
2020-12-05 15:16:28 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -31,11 +31,3 @@ class CustomTransformer(BaseEstimator, TransformerMixin):
         koi_pdisposition
         
         return data
- 
-class ModifiedLabelEncoder(LabelEncoder):
-
-    def fit_transform(self, y, *args, **kwargs):
-        return super().fit_transform(y).reshape(-1, 1)
-
-    def transform(self, y, *args, **kwargs):
-        return super().transform(y).reshape(-1, 1)
